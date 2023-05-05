eb7eb882714ba55cb8114925f1020c4061a3d696
Farhana-Keya <104942229+Farhana-Keya@users.noreply.github.com>
2023-05-05 19:09:07 +0200

    Add files via upload

--- app/services/collect_paper.py
+++ app/services/collect_paper.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-
+#converting string to list
 def keyword_extraction_from_list(keywords):
     if isinstance(keywords,float):
         return 0
@@ -11,36 +11,34 @@ def keyword_extraction_from_list(keywords):
             array.append(keywords.split(",")[i].replace("[", "").replace("'", "").replace(']', "").lstrip())
     return array
 
+
+#for getting one paper which has the most matching keywords
+"""
+input is user keywords
+Output is one paper which has the most matching keywords 
+"""
 def match_paper(keyword_list):
     print("keyword_list ",keyword_list)
     matching_papers = {}
 
-    df = pd.read_csv('/home/farhana/Documents/orkg-email-address-extraction-main./app/services/dataset/author_info_test1_new.csv')
+    df = pd.read_csv('~/Documents/CIMPLE_project/app/services/dataset/author_info_test1_new.csv')
     length = len(df)
     for i in range(length):
 
         c = 0
-        # print("i ",i)
-        # print("before ",df['paper_kewords_from_pubmed'][i])
         author_keywords = keyword_extraction_from_list(df['paper_kewords_from_pubmed'][i])
-        # print("author_keyword ",author_keywords)
         if author_keywords == 0:
             continue
 
         list1 = keyword_list + author_keywords
-        # print("list1 ",list1)
         df1 = pd.value_counts(np.array(list1))
-        # print("df1.index",df1.index)
-        # print("df1.values ",df1.values)
         for j in df1.values:
             if j >= 2:
                 c = c + 1
-        # print("c ",c)
         if c >= 1:
             matching_papers[df['paper_id'][i]] = c
         matching_papers = sorted(matching_papers.items(), key=lambda x: x[1], reverse=True)
         matching_papers = dict(matching_papers)
-        # print("matching papers ", matching_papers)
 
     top_item = None
     top_value = None
@@ -48,10 +46,9 @@ def match_paper(keyword_list):
         top_item = key
         top_value = value
         break
-    # print("top item", top_item)
-    # print("top value", top_value)
+
     row = df[df['paper_id'] == top_item].index[0]
     doi = df['doi'][row]
-    # print("doi ",doi)
 
-    return top_item, top_value,doi
+
+    return top_item, doi
